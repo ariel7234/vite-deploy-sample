0b97df67319962cb6a4b09f81350616389b1fe67
YJ Chen <ariel7234@gmail.com>
2023-02-07 22:00:08 +0800

    init project

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+import { createPinia } from "pinia";
+import { createApp } from "vue";
+import VueAxios from "vue-axios";
+import App from "./App.vue";
+import router from "./router";
+
+import "./assets/main.css";
+
+const app = createApp(App);
+
+app.use(createPinia());
+app.use(router);
+app.use(VueAxios, axios);
+app.mount("#app");
